a1219803d62faf5315a04cb9c70cd07e95bd16aa
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-11-22 19:15:24 +0100

    added functionality for publishing messages

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	rmqCh, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = pubsub.PublishJSON(
+		rmqCh, 
+		routing.ExchangePerilDirect, 
+		routing.PauseKey, 
+		routing.PlayingState{
+			IsPaused: true,
+			},
+		)
+
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+
+	fmt.Println("Pause message sent!")
+
 }
---
+++ internal/pubsub/pub_json.go
@@ -0,0 +1,28 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonBytes, err := json.Marshal(val)
+	if err != nil {
+		log.Printf("error Marshalling value: %v", val)
+		return err
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false,
+	 					amqp.Publishing{
+							ContentType: "application/json",
+							Body: jsonBytes})
+	if err != nil {
+		log.Printf("error publishing message to the exchange")
+		return err
+	}
+
+	return nil
+}
